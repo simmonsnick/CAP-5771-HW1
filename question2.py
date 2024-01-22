9560d8bbc0a4c8143c5fc5b71d4678ddd38371fd
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-22 02:36:08 -0500

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,26 @@
+
+
+def question2_1():
+    #Number of courses registered by student in a given semester
+    answer = ['Discrete', 'Quantitative', 'Ratio']
+    return answer
+
+def question2_2():
+    #Speed of a car(in miles per hour)
+    answer = ['Continuous', 'Quantitative', 'Ratio']
+    return answer
+
+def question2_3():
+    #Decibel as a measure of sound intensity
+    answer = ['Continuous', 'Quantitative', 'Ratio']
+    return answer
+
+def question2_4():
+    #Hurricance intensity according in the Sar-Simpson Hurricane Scale
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    return answer
+
+def question2_5():
+    #Social Security number
+    answer = ['Discrete', 'Qualitative', 'Norminal']
+    return answer
