d5f402cb973e3b01c4cc3f50c59f8f5677f78b22
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-22 05:05:25 -0500

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,44 @@
+
+def question3_1():
+    #Julian Date, which is the number of days elapsed since 12 noon Green-wich Mean Time of January 1, 4713 BC.
+    answer = ['Continuous', 'Quantitative', 'Interval']
+    return answer
+
+def question3_2():
+    #Movie ratings provided by users(1-star, 2-star, 3-star, or 4-star)
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    return answer
+
+def question3_3():
+    #Mood level of a blogger (cheerful, calm, relaxed, bored, sad, angry, or frustrated)
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    return answer
+
+def question3_4():
+    #Average number of hours a user spent on the Internet in a week.
+    #This statement has a ratio and interval attribute. The ratio is the zero timeframe spent. 
+    #The interval is there no true zero point which 3 and 5 hours difference.
+    answer = ['Continuous', 'Quantitative', 'Ratio and Interval']
+    return answer
+
+def question3_5():
+    #IP address of a machine.
+    answer = ['Discrete', 'Qualitative', 'Norminal']
+    return answer
+
+def question3_6():
+    #Richer scale (in terms of energy release during an earthquake).
+    answer = ['Continuous', 'Quantitative', 'Ratio']
+    return answer
+
+def question3_7():
+    #Salary above the median salary of all employees in an organization.
+    #This statement has a ratio and interval attribute. The comparison of individual salaries which is interval.
+    #The indvidual salary could be a zero value.
+    answer = ['Continuous', 'Quantitative', 'Ratio and Interval']
+    return answer
+
+def question3_8():
+    #Undergraduate level(Freshman, Sophomore, Junior, and Senior) for measuring years in college
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    return answer
