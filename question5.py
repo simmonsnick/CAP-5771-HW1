ea357f4f034e0d54e5b334cbdf1d7eab2cebb7a4
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-28 15:27:47 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -5,19 +5,17 @@ def question5_1():
 # the Age attribute into 3 bins
 # Show the userIDs assigned to each of the 3 bins
     answer = {
-            'bin1: [1, 2, 3, 4, 5, 6, 7, 8]'
-            'bin2: [1, 2, 3]',
-            'bin3: [1]'
-    }
+            'bin1': [1, 2, 3, 4, 5, 6, 7, 8],
+            'bin2': [1, 2, 3],
+            'bin3': [1] }
     return answer
 
 def question5_2():
 # Repeat the previous qustion using the equal frequency approach
     answer = {
-            'bin1: [1, 2, 3]'
-            'bin2: [4, 5, 6]',
-            'bin3: [7, 8, 9]'
-    } 
+            'bin1': [1, 2, 3],
+            'bin2': [4, 5, 6],
+            'bin3': [7, 8, 9] } 
     return answer
 
 def question5_3():
@@ -25,8 +23,7 @@ def question5_3():
 #   Specially, choose the bins in such a way that their members are as pure as possible(belonging to the same class)
 #   Class 1: 17, 24, 25, Class 2: 28, 32, 38, Class 3: 39, 49, 68
     answer = {
-            'bin1: [17, 24, 25]'
-            'bin2: [28, 32, 38]',
-            'bin3: [39, 49, 68]'
-    }
+            'bin1': [17, 24, 25],
+            'bin2': [28, 32, 38],
+            'bin3': [39, 49, 68] }
     return answer
