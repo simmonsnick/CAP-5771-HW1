1f87357964fba9122f83b58238d6ccf89be1861b
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-28 15:32:38 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,51 +1,37 @@
 
 
-def question10_1():  
-# Noise could be a problem with count data mainly if the count measurement has errors that affects data models.
-  answer: False
-  return answer
-
 def question10_2():
 # Correlation coefficient would be the best approach with the linear relationship between two sets of real values and ranges -1 to 1.
-    answer: True
-    return answer
+    return True
 
 def question10_3():
 # Sampling is the best option in processing the entire set of data and reducing a time series.
-    answer: True
-    return answer
+    return True
 
 def question10_4():
 # Noise and outliers are separate terms with different aspects of the data attributes.
-    answer: False
-    return answer
+    return False
 
 def question10_5():
 # The outlier represents data values that are important information, noise has irrelevant values that doesn't follow pattern.
-    answer: False
-    return answer
+    return False
 
 def question10_6():
 # A binary attribute has values 0 or 1 is symmetric, An asymmetric binary attribute would involve two distinct values.
-    answer: False
-    return answer
+    return False
 
 def question10_7():
 # The cosine measure of 1, has vectors that are pointing in the same direction.
-    answer: True
-    return answer
+    return True
 
 def question10_8():
 # Ratio variables can be discrete or continuous attributes.
-    answer: False
-    return answer
+    return False
 
 def question10_9():
 # Quantitative variables could be either continous or discrete.
-    answer: False
-    return answer
+    return False
 
 def question10_10():
 # Coverting ordinal variables to asymmetric binary variables may result in a loss information.
-    answer: False
-    return answer
+    return False
