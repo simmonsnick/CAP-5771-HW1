6a74eaf02f21225b26c314bb098bb98fdd5c1ae2
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-28 15:24:18 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,45 +1,45 @@
 
 
 def question3_1():
-    #Julian Date, which is the number of days elapsed since 12 noon Green-wich Mean Time of January 1, 4713 BC.
-    answer = ['Continuous', 'Quantitative', 'Interval']
+# Julian Date, which is the number of days elapsed since 12 noon Green-wich Mean Time of January 1, 4713 BC.
+    answer = ['continuous', 'quantitative', 'interval']
     return answer
 
 def question3_2():
-    #Movie ratings provided by users(1-star, 2-star, 3-star, or 4-star)
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+# Movie ratings provided by users(1-star, 2-star, 3-star, or 4-star)
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question3_3():
-    #Mood level of a blogger (cheerful, calm, relaxed, bored, sad, angry, or frustrated)
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+# Mood level of a blogger (cheerful, calm, relaxed, bored, sad, angry, or frustrated)
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question3_4():
-    #Average number of hours a user spent on the Internet in a week.
-    #This statement has a ratio and interval attribute. The ratio is the zero timeframe spent. 
-    #The interval is there no true zero point which 3 and 5 hours difference.
-    answer = ['Continuous', 'Quantitative', 'Ratio and Interval']
+# Average number of hours a user spent on the Internet in a week.
+# This statement has a ratio and interval attribute. The ratio is the zero timeframe spent. 
+# The interval is there no true zero point which are 2 or 5 hours difference.
+    answer = ['continuous', 'quantitative', 'both']
     return answer
 
 def question3_5():
-    #IP address of a machine.
-    answer = ['Discrete', 'Qualitative', 'Norminal']
+# IP address of a machine.
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
 
 def question3_6():
-    #Richer scale (in terms of energy release during an earthquake).
-    answer = ['Continuous', 'Quantitative', 'Ratio']
+# Richer scale (in terms of energy release during an earthquake).
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_7():
-    #Salary above the median salary of all employees in an organization.
-    #This statement has a ratio and interval attribute. The comparison of individual salaries which is interval.
-    #The indvidual salary could be a zero value.
-    answer = ['Continuous', 'Quantitative', 'Ratio and Interval']
+# Salary above the median salary of all employees in an organization.
+# This statement has a ratio and interval attribute. The comparison of individual salaries which is interval.
+# The indvidual salary could be a zero value.
+    answer = ['continuous', 'quantitative', 'both']
     return answer
 
 def question3_8():
-    #Undergraduate level(Freshman, Sophomore, Junior, and Senior) for measuring years in college
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+# Undergraduate level(Freshman, Sophomore, Junior, and Senior) for measuring years in college
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
