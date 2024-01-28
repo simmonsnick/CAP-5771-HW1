2e30efd7bc33c73a55e4b44df81d959296076da5
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-28 15:29:32 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -2,27 +2,24 @@
 
 def question6_1():
 # The bins will be affected and values may not be the same bins as before.
-    
    answer = {
-    'equal_width': ['No Change', 1, 'Change', 2, 'Change', 3, 'Change', 4, 'Change', 5, 'No Change', 6, 'Change', 7, 'No Change', 8, 'Change', 9, 'No Change', 10],
-    'equal_frequency': ['No Change', 1, 'Change', 2, 'Change', 3, 'Change', 4, 'Change', 5, 'Change', 6, 'Change', 7, 'Change', 8, 'Change', 9, 'Change', 10],
-}
-
+    'equal_width': ['no change', 1, 'change', 2, 'change', 3, 'change', 4, 'change', 5, 'no change', 6, 'change', 7, 'no change', 8, 'change', 9, 'no change', 10],
+    'equal_frequency': ['no change', 1, 'change', 2, 'change', 3, 'change', 4, 'change', 5, 'change', 6, 'change', 7, 'change', 8, 'change', 9, 'change', 10],
+   }
    return answer
 
 def question6_2():
 # Values may change within the bins, distances between the values would change.
     answer = {
-    'equal_width': ['No Change', 1, ' No Change', 2, 'Change', 3, 'Change', 4, 'Change', 5, 'No Change', 6, 'Change', 7, 'Change', 8, 'No Change', 9, 'No Change', 10],
-    'equal_frequency': ['No Change', 1, 'No Change', 2, 'Change', 3, 'Change', 4, 'Change', 5, 'Change', 6, 'No Change', 7, 'Change', 8, 'No Change', 9, 'No Change', 10],   
+    'equal_width': ['no change', 1, ' no change', 2, 'change', 3, 'change', 4, 'change', 5, 'no change', 6, 'change', 7, 'change', 8, 'no change', 9, 'no change', 10],
+    'equal_frequency': ['no change', 1, 'no change', 2, 'change', 3, 'change', 4, 'change', 5, 'change', 6, 'no change', 7, 'change', 8, 'no change', 9, 'no change', 10],   
     }
     return answer
 
 def question6_3():
 # Inversion of the original order of values means means the values in dataset is reversed
     answer = {
-    'equal_width': ['Change', 1, ' No Change', 2, 'Change', 3, 'Change', 4, 'No Change', 5, 'Change', 6, 'No Change', 7, 'No Change', 8, 'Change', 9, 'Change', 10],
-    'equal_frequency': ['Change', 1, 'No Change', 2, 'Change', 3, 'Change', 4, 'Change', 5, 'No Change', 6, 'No Change', 7, 'Change', 8, 'Change', 9, 'Change', 10], 
-        
+    'equal_width': ['change', 1, 'no change', 2, 'change', 3, 'change', 4, 'no change', 5, 'change', 6, 'no change', 7, 'no change', 8, 'change', 9, 'change', 10],
+    'equal_frequency': ['change', 1, 'no change', 2, 'change', 3, 'change', 4, 'change', 5, 'no change', 6, 'no change', 7, 'change', 8, 'change', 9, 'change', 10],   
     }
     return answer 
