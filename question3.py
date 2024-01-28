a61d63b8d4f901a4ea7c5f20a4cee3b9f88b31d2
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-28 16:07:41 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -19,7 +19,7 @@ def question3_4():
 # Average number of hours a user spent on the Internet in a week.
 # This statement has a ratio and interval attribute. The ratio is the zero timeframe spent. 
 # The interval is there no true zero point which are 2 or 5 hours difference.
-    answer = ['continuous', 'quantitative', 'both']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_5():
@@ -36,7 +36,7 @@ def question3_7():
 # Salary above the median salary of all employees in an organization.
 # This statement has a ratio and interval attribute. The comparison of individual salaries which is interval.
 # The indvidual salary could be a zero value.
-    answer = ['continuous', 'quantitative', 'both']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_8():
