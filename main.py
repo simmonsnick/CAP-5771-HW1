f0d56c4a5d5cbb213bc75740400a7459a5208dec
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-01-31 11:20:44 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def question8_1():
+# In Jaccord coefficient the x1, x2 vector are similar
+    answer = ['x1', 'x2']
+    return answer
+
+def question8_2():
+# In Simple matching coefficent x1, x2 vector are similar
+    answer = ['x1', 'x2']
+    return answer
+
+def question8_3():
+# In Simple Euclidean distance both vectors are similar
+    answer = 'equally similar'
+    return answer 
+
+def question8_4():
+# In Simple Euclidean both vectors are similar
+    answer = 'equally similar'
+    return answer
+
